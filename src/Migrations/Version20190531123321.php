89d5c3865e5a86b81b285bb7b17741fc71c85ffb
Theo SIKLI <theosikli@mbp-de-theo-3.home>
2019-05-31 15:22:29 +0200

    fix not allow not field utilisateur

--- src/Migrations/Version20190531123321.php
+++ src/Migrations/Version20190531123321.php
@@ -15,7 +15,7 @@ final class Version20190531123321 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE utilisateur CHANGE token token VARCHAR(255) NOT NULL, CHANGE date_naissance date_naissance DATE NOT NULL');
+        $this->addSql('ALTER TABLE utilisateur CHANGE token token VARCHAR(255), CHANGE date_naissance date_naissance DATE');
     }
 
     public function down(Schema $schema) : void
