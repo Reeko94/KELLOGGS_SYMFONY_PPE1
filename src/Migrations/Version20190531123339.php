2996588984a0b4e872e20e1b508819f094ce3823
Theo SIKLI <theosikli@mbp-de-theo-3.home>
2019-05-31 14:46:11 +0200

    add view migration

---
+++ src/Migrations/Version20190531123339.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190531123339 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE VIEW articlesFabricants as SELECT a.id as aid,f.id as id, f.libelle,a.libelle as designation,a.disponibilite,a.prix,a.calories FROM articles a,  fabricants f where a.fabricant_id = f.id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP VIEW articlesFabricants');
+    }
+}
